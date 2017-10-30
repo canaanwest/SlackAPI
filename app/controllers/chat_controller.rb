21ec239632324c1385aaa9293e9b18e7e7eb312a
Canaan West <canaan.west@gmail.com>
2017-10-30 13:39:15 -0700

    added .env file

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,10 +1,13 @@
 class ChatController < ApplicationController
   def index
+    #lists all slack channels
   end
 
   def new
+    #lets you make a new message for the slack channel
   end
 
   def create
+    #will create the message, post it to the channel, and redirect you back to the channel
   end
 end
